bc128233f6e43c6d165ff7cd632428117954c7c9
MEI YIN LO <luomein@gmail.com>
2023-08-27 15:35:22 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyLibraryGitLab
+
+A description of this package.
---
+++ Sources/MyLibraryGitLab/MyLibraryGitLab.swift
@@ -0,0 +1,6 @@
+public struct MyLibraryGitLab {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
